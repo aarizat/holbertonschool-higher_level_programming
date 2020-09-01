6581438fd4272f859660af82cdf7bfa192e138bc
aarizatr <aarizatr@gmail.com>
2020-09-01 19:00:17 +0000

    Check for digits

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for ch in str:
-        print("{}".format(chr(ord(ch) - 32)), end='')
-    print("\n", end='')
+        print("{}".format(chr(ord(ch) - 32) if ord(ch) in range(97, 123)
+                          else ch), end='')
+    print("")
