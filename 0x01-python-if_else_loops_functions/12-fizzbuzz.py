aa913a535141b644d19dc127bad508c59f5755f7
aarizatr <aarizatr@gmail.com>
2020-09-01 17:56:38 +0000

    Check multiples of 3 and 5

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if not i % 5 and not i % 3:
+            print("FizzBuzz", end='')
+        elif not i % 5:
+            print("Buzz", end='')
+        elif not i % 3:
+            print("Fizz", end='')
+        else:
+            print(i, end='')
+        if i != 100:
+            print(" ", end='')
