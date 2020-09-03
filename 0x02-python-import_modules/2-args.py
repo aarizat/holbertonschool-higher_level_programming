c9fd9b32680c17bdd2763ccc10dcaa50d5804dc1
aarizatr <aarizatr@gmail.com>
2020-09-03 19:26:41 +0000

    Modify msg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
     if argc == 1:
-        print("{} arguments".format(argc - 1))
+        print("{} arguments.".format(argc - 1))
     else:
-        msg = "argument" if argc == 2 else "arguments"
+        msg = "argument:" if argc == 2 else "arguments:"
         print("{} {}".format(argc - 1, msg))
         for idx, arg in enumerate(sys.argv[1:]):
             print("{}: {}".format(idx+1, arg))
