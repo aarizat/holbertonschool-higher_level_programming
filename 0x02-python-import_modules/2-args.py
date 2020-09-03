cb0fd0b4b9bd6d42ff66bf469ffa65f82252e359
aarizatr <aarizatr@gmail.com>
2020-09-03 19:24:14 +0000

    Put msg for argc > 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if argc == 1:
         print("{} arguments".format(argc - 1))
     else:
-        print("{} arguments".format(argc - 1))
+        msg = "argument" if argc == 2 else "arguments"
+        print("{} {}".format(argc - 1, msg))
         for idx, arg in enumerate(sys.argv[1:]):
             print("{}: {}".format(idx+1, arg))
