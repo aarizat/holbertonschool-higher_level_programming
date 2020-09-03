13bf174a6831eefbab46114b8898189e85e24481
aarizatr <aarizatr@gmail.com>
2020-09-03 23:19:02 +0000

    Delete argc variable

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    argc = len(sys.argv)
-    if argc != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] not in "+-*/":
