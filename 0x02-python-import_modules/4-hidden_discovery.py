4e0fb9f5e8886c2e3888a01e5e25604a8b1b8d71
aarizatr <aarizatr@gmail.com>
2020-09-04 01:47:56 +0000

    come back first option

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print(*[n if n[:2] != "__" for n in dir(hidden_4)], sep="\n")
+    print(*list(filter(lambda n: n[0] != "_", dir(hidden_4))), sep="\n")
