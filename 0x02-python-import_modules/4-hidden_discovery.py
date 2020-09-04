49aef242e65081c98fe66c18729910e3b383d8ce
aarizatr <aarizatr@gmail.com>
2020-09-04 01:43:02 +0000

    Use list comprehension

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print(*list(filter(lambda n: n[:2] != "__", dir(hidden_4))), sep="\n")
+    print(*[n if n[:2] != "__" for n in dir(hidden_4)], sep="\n")
