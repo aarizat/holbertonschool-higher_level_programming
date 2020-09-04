b964c12883be53824bfd3f80066473bbfc358fe6
aarizatr <aarizatr@gmail.com>
2020-09-04 02:06:38 +0000

    fix bug 1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print(*[name for name in dir(hidden_4) if name[:2] != '__'], sep="\n")
+    for name in dir(hidden_4):
+        if name[0:2] != '__':
+            print(name)
