53e0f4c858f04f1b6cd7a75f4eef555bdeddd9f8
aarizatr <aarizatr@gmail.com>
2020-09-21 22:02:12 +0000

    Improve readability of the code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        elements = 0
-        for idx in range(x):
+    elements = 0
+    for idx in range(x):
+        try:
             print("{}".format(my_list[idx]), end="")
             elements += 1
-        print("")
-        return elements
-    except:
-        print("")
-        return elements
+        except:
+            pass
+    print("")
+    return elements
