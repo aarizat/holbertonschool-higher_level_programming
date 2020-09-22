8a7752da15d728208823a3ba1f9e591833e8d072
aarizatr <aarizatr@gmail.com>
2020-09-22 21:13:03 +0000

    Set setter and property for square class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Define a square class
+"""
+
+
+class Square:
+    """Create an instance of a square object
+
+    Attributes:
+        size: integer number
+    """
+    def __init__(self, size=0):
+        """ Initialize square object
+
+        Args:
+            size (int): side of the square
+       """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
