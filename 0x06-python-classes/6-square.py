18d617b5d677a0b77d20380a27b51bfd2d7a2436
aarizatr <aarizatr@gmail.com>
2020-09-22 23:15:48 +0000

    Add condition for check negative numbers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,9 @@ class Square:
             size (int): side of the square
             position (tuple): init position of the square.
        """
-        if len(position) != 2 or not isinstance(position[0], int) or not isinstance(position[1], int):
+        if (len(position) != 2 or not isinstance(position[0], int) or
+            not isinstance(position[1], int) or position[0] < 0 or
+            position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -44,7 +46,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if len(value) != 2 or not isinstance(value[0], int) or not isinstance(value[1], int):
+        if (len(value) != 2 or not isinstance(value[0], int) or
+            not isinstance(value[1], int) or value[0] < 0 or
+            value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
