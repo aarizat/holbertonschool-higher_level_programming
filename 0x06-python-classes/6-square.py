5657adfd1376769f1ca15e0094d653bca9f40da5
aarizatr <aarizatr@gmail.com>
2020-09-22 23:03:52 +0000

    Check if the coords are integer objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
             size (int): side of the square
             position (tuple): init position of the square.
        """
-        if len(position) != 2:
+        if len(value) != 2 or not isinstance(position[0], int) or not isinstance(position[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -44,7 +44,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if len(value) != 2:
+        if len(value) != 2 or not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
