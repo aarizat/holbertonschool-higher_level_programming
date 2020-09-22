d63d14ef6f8c0f45fbc94b6d43ded7b91489d513
aarizatr <aarizatr@gmail.com>
2020-09-22 23:44:37 +0000

    Add conditions inside init method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,6 +18,14 @@ class Square:
             size (int): side of the square
             position (tuple): init position of the square.
        """
+        if (len(position) != 2 or not isinstance(position[0], int) or
+            not isinstance(position[1], int) or position[0] < 0 or
+            position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
