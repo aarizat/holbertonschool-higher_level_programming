68905d58a703064e3437eeb48d63fdb68e4c79e4
aarizatr <aarizatr@gmail.com>
2020-09-22 22:38:07 +0000

    Add method for printing square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Define a square class
+"""
+
+
+class Square:
+    """Create an instance of a square object
+
+    Attributes:
+        size: integer number
+    """
+    def __init__(self, size=0):
+        """ Initialize square object
+
+        Args:
+            size (int): side of the square
+       """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
