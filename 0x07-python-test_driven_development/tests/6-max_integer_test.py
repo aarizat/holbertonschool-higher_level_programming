ccd440b7e0774227693bfe5069ca00b810205a05
aarizatr <aarizatr@gmail.com>
2020-09-26 05:15:19 +0000

    Another test case

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
+    """
+    Test cases to max integer into a list.
+    """
 
     def test_output(self):
         self.assertEqual(max_integer([4, 5, 6, 8]), 8)
@@ -13,6 +19,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-10, -20, -30]), -10)
         self.assertEqual(max_integer([10, 10, 10]), 10)
         self.assertEqual(max_integer([2.5, 1.6, 10.8]), 10.8)
+        self.assertEqual(max_integer([2, 1.6, 10.8, 20]), 20)
 
     def test_bool(self):
         self.assertTrue(max_integer([True, False, True]), True)
@@ -20,4 +27,6 @@ class TestMaxInteger(unittest.TestCase):
     def test_str(self):
         self.assertEqual(max_integer(["Hi", "Hello"]), "Hi")
         self.assertEqual(max_integer("Hi"), "i")
- 
+
+if __name__ == '__main__':
+        unittest.main()
