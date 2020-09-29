435b9c4d4b9f6223bb8e7e549b4827f794d51a19
aarizatr <aarizatr@gmail.com>
2020-09-29 00:40:51 +0000

    Fix pep8 style

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,11 +5,11 @@ Module to create a rectangle class.
 
 
 class Rectangle:
-    number_of_instances = 0
-
     """
     The instance of this class creates Rectangle objects.
     """
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Init rectangle objects"""
