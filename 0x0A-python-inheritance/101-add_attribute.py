21b731f4850f05513d799206c72884a44810a82c
aarizatr <aarizatr@gmail.com>
2020-10-06 21:55:00 +0000

    Add attributes to an object

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def add_attribute(obj, name, value):
+    """
+    Add an attribute to object if it's possible.
+
+    Args:
+       obj (object): object
+       name (str): name of the attribute to add to object.
+       value (str): value to set in the attribute.
+
+    Return:
+       nothing.
+    """
+    if '__dict__' not in dir(obj):
+        raise TypeError("can't add new attribute")
+    obj.__dict__[name] = value
