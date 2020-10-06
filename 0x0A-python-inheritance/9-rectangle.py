df19082ee4719373114cf688addeac1de2cc172f
aarizatr <aarizatr@gmail.com>
2020-10-06 21:51:32 +0000

    Add methods to Rectangle class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Define a rectagle class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    init rectangle objects.
+    """
+    def __init__(self, width, height):
+        """
+        Init object attributes.
+
+        Args:
+            width (int): integer number greater than zero
+            height (int): integer number greater than zero.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Return the area of a rectangle.
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Printable object.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
