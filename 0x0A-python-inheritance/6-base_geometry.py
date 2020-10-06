9539830ee7df89c0f6562aef771b0491d984d36b
aarizatr <aarizatr@gmail.com>
2020-10-06 21:48:07 +0000

    Add public method to BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define Geometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    create object BaseGeometry
+    """
+    def area(self):
+        """
+        Raise a exception.
+        """
+        raise Exception("area() is not implemented")
