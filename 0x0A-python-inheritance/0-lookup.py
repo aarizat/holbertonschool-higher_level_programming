31d263da44e63c4d7b979b047ee63d84260a122e
aarizatr <aarizatr@gmail.com>
2020-10-06 21:40:17 +0000

    Show attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that retunr all of atttrbutes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """
+    Return a list with all of attributes and methods of an object.
+    """
+    return dir(obj)
