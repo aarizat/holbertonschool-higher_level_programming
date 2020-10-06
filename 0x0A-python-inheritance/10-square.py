9d99b50c386996950dd2d1fa602b6284ee3615c5
aarizatr <aarizatr@gmail.com>
2020-10-06 21:51:57 +0000

    Create Square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Define square class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    init square objects.
+    """
+    def __init__(self, size):
+        """
+        init attributes.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """"
+        Return the area of a square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Printable square.
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
