795fb775f9fde41907dc50121091a79630cb8bbc
aarizatr <aarizatr@gmail.com>
2020-10-09 16:54:37 +0000

    Define base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Define Base class.
+"""
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize class Base.
+
+        Args:
+            id (int): integer number. Default to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
