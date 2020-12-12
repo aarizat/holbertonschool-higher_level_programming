5e3c082be6afd376a8977b63581db4f349f4733a
aarizat <aarizatr@gmail.com>
2020-12-12 21:15:45 +0000

    Filter states startwith 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from
+the database hbtn_0e_0_usa.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+    with db.cursor() as cur:
+        cur.execute('''SELECT * FROM states
+                       WHERE name LIKE "N%"
+                       ORDER BY id ASC''')
+        [print(s) for s in cur.fetchall()]
