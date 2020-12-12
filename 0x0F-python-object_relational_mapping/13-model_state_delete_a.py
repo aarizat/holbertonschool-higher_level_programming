998ad751813ccd624a4f09a77a07c19110debfa3
aarizat <aarizatr@gmail.com>
2020-12-12 22:59:18 +0000

    Put commit statement out of the for loop

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     states = session.query(State).filter(State.name.contains('a')).all()
     for state in states:
         session.delete(state)
-        session.commit()
+    session.commit()
