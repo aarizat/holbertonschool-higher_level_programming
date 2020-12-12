2fe2576b130e67b7caeed134f7e01f68fe4ff96c
aarizat <aarizatr@gmail.com>
2020-12-12 22:43:48 +0000

    Delete unnecessary lines

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,14 +3,9 @@
 contains the class definition of a State and an instance
 Base = declarative_base()
 """
-import sys
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
-
-user, psswd, db = sys.argv[1], sys.argv[2], sys.argv[3]
-engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                       .format(user, psswd, db), pool_pre_ping=True)
 
 Base = declarative_base()
 
