e76d1922739f8b014f1a7f6cb562eb1656990376
aarizat <aarizatr@gmail.com>
2021-01-12 01:14:57 +0000

    Create request object and fix typo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as resp:
         body = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf-8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
