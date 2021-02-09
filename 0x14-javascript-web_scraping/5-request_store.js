ddcc6b20babe793e9b2ee9703865c60b85371c44
aarizat <aarizatr@gmail.com>
2021-02-09 20:52:12 +0000

    Fix semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,8 +2,12 @@
 const request = require('request');
 const fs = require('fs');
 request(process.argv[2], (err, res, body) => {
-  if (err) return;
+  if (err) {
+    console.error(err);
+  }
   fs.writeFile(process.argv[3], body, 'utf8', err => {
-    if (err) return;
+    if (err) {
+      console.error(err);
+    }
   });
 });
