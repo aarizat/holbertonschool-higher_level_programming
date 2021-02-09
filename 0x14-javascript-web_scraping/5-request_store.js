280fe2d9715b127af5871f905f841a1d086d4020
aarizat <aarizatr@gmail.com>
2021-02-09 20:47:58 +0000

    Manage errors

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,5 +3,7 @@ const request = require('request');
 const fs = require('fs');
 request(process.argv[2], (err, res, body) => {
   if (err) return;
-  fs.writeFile(process.argv[3], body, 'utf8');
+  fs.writeFile(process.argv[3], body, 'utf8', err => {
+    if (err) return;
+  });
 });
