eeb524cbc10585525e44e53c081d178f83504403
aarizat <aarizatr@gmail.com>
2021-02-09 18:08:33 +0000

    Get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (error) return;
+  console.log(`code: ${response.statusCode}`);
+});
