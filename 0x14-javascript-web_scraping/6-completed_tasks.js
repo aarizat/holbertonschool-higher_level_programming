9289a1e0275ce2967f46b9d2e4d4e4037be16667
aarizat <aarizatr@gmail.com>
2021-02-09 21:35:06 +0000

    Fix requirements

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,16 +2,14 @@
 const req = require('request');
 const completed = {};
 req(process.argv[2], (err, res, body) => {
-  if (err) {
-    console.error(err);
-  }
-  const list = JSON.parse(body);
-  for (const item of list) {
-    if (completed[item.userId] === undefined) {
-      completed[item.userId] = 0;
-    }
-    if (item.completed) {
-      completed[item.userId]++;
+  if (!err) {
+    const list = JSON.parse(body);
+    for (const item of list) {
+      if (item.completed && completed[item.userId] === undefined) {
+        completed[item.userId] = 1;
+      } else if (item.completed) {
+        completed[item.userId] += 1;
+      }
     }
   }
   console.log(completed);
