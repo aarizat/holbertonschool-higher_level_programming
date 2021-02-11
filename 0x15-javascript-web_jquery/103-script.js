26d0d58d92fef5deee25dade0e35283e96fcd1d6
aarizat <aarizatr@gmail.com>
2021-02-11 02:03:22 +0000

    remove 103-script file

--- 0x15-javascript-web_jquery/103-script.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(function () {
-  $('INPUT#btn_translate').click(function () {
-    const code = $('INPUT#language_code').val();
-    $.get('https://fourtonfish.com/hellosalut/?' + $.param({ lang: code }), function (data) {
-      $('DIV#hello').text(data.hello);
-    });
-  });
-});
